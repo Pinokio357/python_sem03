a0d32b67974537265b3ea75bd2f40f105f2bf7dd
ivanH <nigga.2023@mail.ru>
2023-02-24 19:45:11 +0300

    second task

---
+++ ex2.py
@@ -0,0 +1,23 @@
+﻿
+
+
+dict_bonus ={1: ["a", "e", "i", "o", "u", "l", "n", "s", "t", "r", "а", "в", "е", "и", "н", "о", "р", "с", "т"],
+         2: ["d", "g", "д", "к", "л", "м", "п", "у"],
+         3: ["b", "c", "m", "p", "б", "г", "ё", "ь", "я"],
+         4: ["f", "h", "v", "w", "y", "й", "ы"],
+         5: ["k", "ж", "з", "х", "ц", "ч"],
+         8: ["j", "x", "ш", "э", "ю"],
+         10: ["q", "z", "ф", "щ", "ъ" ]}
+list_bonus = [1, 2, 3, 4, 5, 8, 10]
+word = input("enter word: ")
+bonus_sum = 0
+for i in word:
+    for bonus in dict_bonus:
+        for letter in dict_bonus[bonus]:
+            if letter == i:
+                bonus_sum += bonus
+
+print(f"point count = {bonus_sum}")
+
+
+
